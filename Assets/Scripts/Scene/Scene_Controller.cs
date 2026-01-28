2b3e435f31d6bd088bb4224bb89d2fa795781dd4
Ercan Guvenc <duuloic@gmail.com>
2026-01-28 16:25:27 +0200

    replaced scene change with level change with teleportation

--- Assets/Scripts/Scene/Scene_Controller.cs
+++ Assets/Scripts/Scene/Scene_Controller.cs
@@ -1,25 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Cinemachine;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Scene_Controller : MonoBehaviour
 {
-    [SerializeField] string nextScene;
+    [Header("Target Level Settings")]
+    [SerializeField] Transform spawnPoint;
+    
+    [Header("Target Level Camera Settings")]
+    [SerializeField] CinemachineConfiner2D cameraConfiner;
+    [SerializeField] GameObject cameraBoundsObj;
 
+    [Header("Overlay")]
+    [SerializeField] GameObject overlay;
+
+
+    #region SCENE_GATE_TRIGGER
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("To: " + nextScene);
-        ChangeScene(nextScene);
+        SetOverlayState(true);
+        TeleportToLevel(spawnPoint, collision.gameObject);
+        SetCameraBounds(cameraBoundsObj, cameraConfiner);
+        SetOverlayState(false);
     }
+
+    #endregion
 
     #region SCENE_GATE_CONTROLS
 
-    private void ChangeScene(string scene)
+    private void TeleportToLevel(Transform targetPos, GameObject player)
     {
-        SceneManager.LoadScene(scene);
+        print("targetPos:" + targetPos.position);
+         print("playerPos:" + player.transform.position);
+        player.transform.position = targetPos.position;
     }
 
+    #endregion
+
+    #region SCENE_CAMERA_CONTROLS
+
+    private void SetCameraBounds(GameObject boundsObj, CinemachineConfiner2D confiner)
+    {
+        confiner.m_BoundingShape2D = boundsObj.GetComponent<PolygonCollider2D>();
+    }
+
+    #endregion
+
+    #region OVERLAY_CONTROLS
+
+    private void SetOverlayState(bool state)
+    {
+        overlay.SetActive(state);
+    }
 
     #endregion
 }
