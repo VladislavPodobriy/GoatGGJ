d42719f3a32818a6d3f015b445a3759876eb4b61
Ercan Guvenc <duuloic@gmail.com>
2026-01-31 19:52:45 +0200

    Added inventory UI logic, combined with pickup+drop

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -1,19 +1,56 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
+[Serializable]
+public class InventorySlot
+{
+    public Image image;
+    public string title;
+}
+
 public class InventoryManager : MonoBehaviour
 {
     public List<ItemEntry> items = new List<ItemEntry>();
+    public List<InventorySlot> slots = new List<InventorySlot>();
+
+    public Canvas InventoryCanvas;
+
+    private void Start()
+    {
+        SetupInventorySlots();
+    }
 
     private void Update()
     {
         CheckShouldDrop();
+        SwitchInventoryWindow();
     }
 
     #region INVENTORY_ACTIONS
+
+    private void SwitchInventoryWindow()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+            InventoryCanvas.gameObject.SetActive(!InventoryCanvas.gameObject.activeSelf);
+    }
+
+    private void SetupInventorySlots()
+    {
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Inventory_Slot_Icon"))
+        {
+            slots.Add(new InventorySlot
+            {
+                image = obj.GetComponent<Image>(),
+                title = "",
+            });
+        }
+
+        InventoryCanvas.gameObject.SetActive(false);
+    }
 
     public void Add(Item item)
     {
@@ -25,14 +62,23 @@ public class InventoryManager : MonoBehaviour
             data = item.data,
         });
 
+        int slotIndex = items.Count - 1;
+        if (slotIndex < slots.Count)
+        {
+            slots[slotIndex].image.sprite = item.data.icon;
+            slots[slotIndex].title = item.data.title;
+        }
+
         item.gameObject.SetActive(false);
     }
 
     public void Remove(Item item)
     {
-        print("Remove from Items"+ item.data.name);
-
         items.RemoveAll(e => e.item == item);
+
+        InventorySlot slot = slots.Where(e => e.title == item.data.title).First();
+        slot.image.sprite = null;
+        slot.title = null;
     }
 
     void CheckShouldDrop()
@@ -41,7 +87,18 @@ public class InventoryManager : MonoBehaviour
         if (selectedEntry != null) Drop(selectedEntry.item);
     }
 
-    void Drop(Item item)
+    public void DropFirst()
+    {
+        Item item = items.First().item;
+
+        item.gameObject.SetActive(true);
+        item.data.isSelected = false;
+        item.transform.position = transform.position;
+
+        Remove(item);
+    }
+
+    public void Drop(Item item)
     {
         item.gameObject.SetActive(true);
         item.data.isSelected = false;
